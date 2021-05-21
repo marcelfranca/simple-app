87a5f5474f7281a0ca9c622f216b0148e37dd056
marcelfranca <marcel.arthur@live.com>
2021-05-21 14:36:42 -0300

    opentracing and jaeger

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask
+from flask import request
+import requests
+
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def pull_requests():
+    # Fetch a list of pull requests on the opentracing repository
+    github_url = "https://api.github.com/repos/opentracing/opentracing-python/pulls"
+    r = requests.get(github_url)
+
+    json = r.json()
+    pull_request_titles = map(lambda item: item['title'], json)
+
+    return 'OpenTracing Pull Requests: ' + ', '.join(pull_request_titles)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+def initialize_tracer():
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1}
+        },
+        service_name='hello-world')
+    return config.initialize_tracer()  # also sets opentracing.tracer
+
+
+flask_tracer = FlaskTracer(initialize_tracer, True, app)
